be6aee140873627bc3d20d2971a4f8062b6eac55
thulasidharan96 <thulasidharankvp@gmail.com>
2025-02-01 10:26:12 +0530

    API Routes Optimization

--- api/routes/attendance.js
+++ api/routes/attendance.js
@@ -6,6 +6,26 @@ const checkAuth = require("../middleware/check-auth");
 
 const StudentAttendance = require("../models/studentAttendance");
 
+router.get("/:userId", checkAuth, (req, res, next) => {
+  const userId = req.params.userId;
+  StudentAttendance.find({ userId: userId })
+    .exec()
+    .then(docs => {
+      console.log("From database", docs);
+      if (docs) {
+        res.status(200).json(docs);
+      } else {
+        res.status(404).json({
+          message: "No valid entry found for provided userId"
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
 router.post("/", checkAuth, (req, res, next) => {
   const studentAttendance = new StudentAttendance({
     _id: new mongoose.Types.ObjectId(),
