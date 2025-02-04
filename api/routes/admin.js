655b74e5936c717f8527c4e2c174e9bae8b9c313
thulasidharan96 <thulasidharankvp@gmail.com>
2025-02-04 18:21:30 +0530

    Add New admin Routes For Frontend Features

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -114,5 +114,32 @@ router.get("/:registrationNumber", checkAuth, checkadmin, (req, res, next) => {
     });
 });
 
+// Get attendance by department and date
+router.get("/department/:department", checkAuth, checkadmin, (req, res, next) => {
+  const department = req.params.department;
+  console.log(req.params);
+  
+  StudentAttendance.find({ department: department }) 
+    .select("userId attendanceStatus _id dateOnly department name registrationNumber")
+    .exec()
+    .then((docs) => {
+      if (!docs || docs.length === 0) {
+        return res.status(404).json({
+          message: "No attendance records found for the given department",
+        });
+      }
+      res.status(200).json({
+        message: "Attendance records fetched successfully",
+        records: docs,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "An error occurred while fetching attendance records",
+        error: err,
+      });
+    });
+});
+
 
 module.exports = router;
