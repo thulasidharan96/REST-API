0d1b8205f4029e4e62a0812ebf8747be76db9789
thulasidharan96 <thulasidharankvp@gmail.com>
2025-02-05 10:28:27 +0530

    new routes

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 const moment = require("moment-timezone");
 const StudentAttendance = require("../models/studentAttendance");
+const User = require("../models/user");
 const checkAuth = require("../middleware/check-auth");
 const checkadmin = require("../middleware/checkadmin");
 
@@ -126,6 +127,34 @@ router.get("/department/:department", checkAuth, checkadmin, (req, res, next) =>
       if (!docs || docs.length === 0) {
         return res.status(404).json({
           message: "No attendance records found for the given department",
+        });
+      }
+      res.status(200).json({
+        message: "Attendance records fetched successfully",
+        records: docs,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "An error occurred while fetching attendance records",
+        error: err,
+      });
+    });
+});
+
+
+// Get User Info by UserId
+router.get("/search/:userId", checkAuth, checkadmin, (req, res, next) => {
+  const userId = req.params.userId;
+  console.log(req.params);
+  
+  User.find({ RegisterNumber: userId })
+    .select("userId _id email department name registrationNumber")
+    .exec()
+    .then((docs) => {
+      if (!docs || docs.length === 0) {
+        return res.status(404).json({
+          message: "No attendance records found for the given userId",
         });
       }
       res.status(200).json({
